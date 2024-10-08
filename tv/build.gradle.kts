9fc5ae311045a1b7cd4ec9aabc8b55bb34c1b54f
raspabamos <raspabamos333@proton.me>
2024-10-09 02:53:54 +0300

    add support for Discord

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.nodpi.hello"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         multiDexEnabled = true
         signingConfig = signingConfigs.getByName("debug")
         splits {
