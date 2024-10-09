0ed17ef414db89e0247b0a0451bc1ca58175aa06
raspabamos <raspabamos333@proton.me>
2024-10-09 14:20:59 +0300

    Улучшен способ перебора вариантов для обхода блокировок YouTube и Discord

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.nodpi.hello"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         multiDexEnabled = true
         signingConfig = signingConfigs.getByName("debug")
         splits {
