58a41cfa09b5da95eabf9988fa2d6e0107ceb3ee
raspabamos <raspabamos333@proton.me>
2024-10-10 14:50:48 +0300

    Улучшен обход блокировок YouTube и Discord в сетях мобильных операторов

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.nodpi.hello"
         minSdk = 21
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
         multiDexEnabled = true
         signingConfig = signingConfigs.getByName("debug")
         splits {
